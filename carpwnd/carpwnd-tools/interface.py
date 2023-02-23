c5896ab65ba0c73cc7eef3ce6a7df253639c1f96
CrashOveride <dcmaccurdy@protonmail.com>
2023-02-22 18:01:12 -0700

    Fixed interface.py and moved back into main

--- carpwnd/carpwnd-tools/interface.py
+++ carpwnd/carpwnd-tools/interface.py
@@ -4,21 +4,25 @@ import subprocess
 import can
 import os
 
-def run_cmd(cmd:string):
+def run_cmd(cmd):
     subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
 
 
 @click.group()
 def cli():pass
 
-@cli.command("can-type",type=click.choice(can.interface), case_sensitive=True)
-@cli.argument("name")
-@cli.option("-b", "--bit-rate", type=click.INT, default=0)
-@cli.argument()
-def change_interface(can_type,name,bit_rate):
-    
-    if (os_version := platform.system()) is "Linux":
-        if can_type is "vcan":
+@cli.command()
+@click.option("-b", "--bit-rate", help="Sets the speed of the interface", type=int, default=0)
+@click.argument("can-type",type=click.Choice(can.VALID_INTERFACES, case_sensitive=True))
+@click.argument("name")
+def setup(can_type, name, bit_rate):
+    '''
+    'can-type' The python-can compatible interface to setup
+
+    'NAME' The name to set the interface as
+    '''
+    if (os_version := platform.system()) == "Linux":
+        if can_type == "vcan":
             run_cmd(f"sudo modprobe {can_type}")
         if name in os.listdir('/sys/class/net/'):
             run_cmd(f"sudo ip link down {name}")
@@ -32,7 +36,7 @@ def change_interface(can_type,name,bit_rate):
 
             run_cmd(f"sudo ip link up {name}")
 
-   if (os_version := platform.system()) is "Darwin":
+    if (os_version := platform.system()) == "Darwin":
        pass
-   if (os_version := platform.system()) is "Windows":
+    if (os_version := platform.system()) == "Windows":
        pass
