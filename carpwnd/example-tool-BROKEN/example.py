59c05ad760de96eb227954103b2791d5e9189625
CrashOverid3 <dcmaccurdy@protonmail.com>
2023-03-03 17:35:45 -0700

    Updated new example module

--- carpwnd/example-tool-BROKEN/example.py
+++ carpwnd/example-tool-BROKEN/example.py
@@ -1,74 +1,103 @@
-#Example carpwnd module writen to help others understand how to write modules
+#Capture Module with excessive notes to help others create modules themselves
 
-#import modules
-import can
-import time
-#all modules require at the very least click
+#import Modules
 import click
+import datetime
+import can 
 
-#ANSI colors
-white = '[95m'
-black = '[94m'
-red = '[96m'
-green = '[92m'
-blue = '[93m'
-endc = '[0m'
+#Setting File extensions to be used by the "-e" click option.
+FILE_EXTENSION = ["asc","blf","csv","db","log","txt"]
 
-#set list of choices for first command option
-colors = ['white', 'black', 'red', 'green', 'blue']
-
-#set click group that carpwnd pulls from to get the module commands
+#Setting a group in click so each module can be pulled back to the main project.
 @click.group()
+
+#setting the click command names.  **Note, The options and arguments for each command are still using 'click' not 'cli'
 def cli():pass
 
-#set first command
+#First click command with all of its options. Everything from here until the first function will be accosiated with it.
 @cli.command()
-#set options for first command
-@click.option("-c", "--channel", help="will give interface name to be identified by")
-
-#set arguments for first command
-#@click.argument("color", type=click.Choice(colors, case_sensitive=True))
-@click.argument("bus-type",type=click.Choice(can.VALID_INTERFACES,case_sensitive=True))
-
-def colorprint(channel, color, bus_type):
+#all of the options are not mandatory and have no order.
+@click.option("-e","--extension-type", help="extension to use for output file", type=click.Choice(FILE_EXTENSION, case_sensitive=True))
+@click.option("-v", "--verbose", help="ouput to stdout while writing to file", is_flag=True)
+#all of the arguments are mandatory and passed in order that they are written here.
+@click.argument("bus-type",type=click.Choice(can.VALID_INTERFACES,case_sensitive=True), default="log")
+@click.argument("channel")
+@click.argument("output-file")
+#The first actual command.  The name of the function is what the command will be named in carpwnd.
+#**Note, The options and arguments have '-' replaced with '_' when passed through.
+def store(extension_type,verbose,bus_type,channel,output_file):
+    #This is the description of the command.
     """
-    'example colorprint' will output the captured frames to stdout in COLOR!
+        'capture store'  command will dump can data into file specified
+
+        output-file file to dump can data capture into
+        
     """
-    buffer = can.BufferedReader()
+
+    #Set a variable to track the ammount of packets captured.
     captured_total = 0
-    bus = can.ThreadSafeBus(interface=bus_type, channel=channel) if channel is not None else can.ThreadSafeBus(interface=bus_type)
-    
+    #Combine the output file and extension type unless no extension type was provided.  Then, place it in the 'file_name' variable.
+    file_name = f"{output_file}.{extension_type}" if extension_type else output_file
+    #Set Python-can bus with type and channel
+    bus = can.ThreadSafeBus(interface=bus_type, channel=channel) 
+    #Create Python-can logger for writing packets to file.
+    write_logger = can.Logger(file_name) if extension_type else open(file_name, "w")
+    #if verbose option was called then dump all packets to terminal aswell
+    if verbose:
+        print_logger = can.Logger()
+    #Create infinite loop for getting packets and writing them to file.
     while True:
         try:
-            msg = buffer.get_message
-            print(f"{color}{msg} {endc}")
-        except KeyboardInterrupt:
-            print(f"{color}{captured_total} captured frames {endc}")
-            logger.stop()
+            #get packet
+            frame = bus.recv()
+            if verbose:
+                print_logger(frame)
+            #Write packet to extension type
+            if extension_type:
+                write_logger(frame)
+            #if extension type was not defined then dump to the same format as candump
+            else:
+                time =datetime.datetime.fromtimestamp(frame.timestamp)
+                id = hex(frame.arbitration_id)
+                frame_len = frame.dlc
+                
+                data = ' '.join(format(x, '02x') for x in frame.data)
+                frame_str = f"({time}) {channel} {id} [{frame_len}] {data}"
+                write_logger.write(frame_str)
+        #Escape loop when user hits ctl+c     
+        except click.exceptions.Abort:
+            #Get the size in MB of the written output file and print for user
+            file_size = can.io.canutils.CanutilsLogWriter.file_size(write_logger)
+            print(f"\n{captured_total}\n{file_size}\tMB written to {file_name}")
+            #Stop and close all of the loggers called
+            if extension_type:
+                write_logger.stop()
+            else:
+                write_logger.close()
+
+            if verbose:
+                print_logger.stop()
             break
 
-        captured_total += 1 
+    captured_total += 1
 
-#set second command
+#The second click command.
 @cli.command()
-#set options for second command
-@click.option("-c", "--channel", help="will give interface name to be identified by")
-
-#set arguments for second command
+#Second commands arguments
 @click.argument("bus-type",type=click.Choice(can.VALID_INTERFACES,case_sensitive=True))
-
-def print(channel, bus_type):
+@click.argument("channel")
+#The second actual command.
+def print(bus_type, channel):
     """
-    'example print' will output the captured frames to stdout
+    'capture print' will output the captured frames to stdout
     """
     logger = can.Printer()
     captured_total = 0
-    bus = can.ThreadSafeBus(interface=bus_type, channel=channel) if channel is not None else can.ThreadSafeBus(interface=bus_type)
-    
+    bus = can.ThreadSafeBus(interface=bus_type, channel=channel)   
     while True:
         try:
             logger(bus.recv())
-        except KeyboardInterrupt:
+        except click.exceptions.Abort:
             print(f"{captured_total} captured frames")
             logger.stop()
             break
