05a81ee474a369844dc200b426732471e4fc9bc3
CrashOverid3 <dcmaccurdy@protonmail.com>
2023-03-07 14:08:05 -0700

    Added replay_dump injection command

--- carpwnd/carpwnd-tools/injection.py
+++ carpwnd/carpwnd-tools/injection.py
@@ -2,6 +2,7 @@ import click
 import can
 import sys
 import os
+import time
 
 @click.group()
 def cli():pass
@@ -39,3 +40,35 @@ def inject_frame(period, duration,bus_type,channel, arbitration_id, data):
             bus.stop()
     else:
         bus.send(msg)     
+
+@cli.command()
+@click.argument("bus-type",type=click.Choice(can.VALID_INTERFACES,case_sensitive=True))
+@click.argument("channel")
+@click.argument("input-file", type=click.Path())
+
+
+def replay_dump(bus_type, input_file, channel):
+    """
+    replay_dump will inject all of the packets on a dump file into the interface
+
+
+    """
+    bus = can.ThreadSafeBus(interface=bus_type, channel=channel)
+    input_file = open(input_file).readlines()
+    for i in input_file:
+        packets = []
+        data = i.split(':')
+        packets.append(data[1][1:18])
+        packets.append('0x'+data[2][2:5])
+        packets.append(data[3][7:30])
+        array = bytearray.fromhex(packets[2])
+        try:
+            print(f"Sending {packets[1]} {packets[2]}")
+            msg = can.Message(arbitration_id=int(packets[1],16),data=array, check=True, is_extended_id=False)
+            bus.send(msg)
+        except ValueError:
+            print('error')
+            sys.exit(-1)
+
+        time.sleep(0.001)
+    print('Packets Sucessfully Sent!')
