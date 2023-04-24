fdc424702e36de6bd813713013def1bb04a8bfe2
CrashOverid3 <dcmaccurdy@protonmail.com>
2023-04-24 12:10:31 -0600

    Some additions before fixing things

--- carpwnd/carpwnd-tools/convert.py
+++ carpwnd/carpwnd-tools/convert.py
@@ -25,18 +25,47 @@ def cli():pass
 @click.option("-f", "--filter-for", help='filter by name')
 @click.option("-v", "--verbose", help='prints to stdout', is_flag=True)
 
-def tofile(input_file, output_file, filter_by, filter_for, verbose):
+def database(input_file, output_file, filter_by, filter_for, verbose):
     '''
-    'convert tofile'  Converts input file to a different format and writes to output
+    'convert database'  Converts input database file to a different format and writes to output
 
-    input-file      the database input file                               
-    output-file     the output for the converted database file
+    input-file          the database input file                               
+    output-file         the output for the converted database file
     '''
     db = loadfile(input_file, filter_by, filter_for)
     if verbose == True: print(db)
     cantools.database.dump_file(db, output_file)
     print(f"Database written to {output_file}")
     pass
+
+@cli.command()
+@click.argument("input-file", type=click.Path())
+@click.argument("output-file")
+@click.option("-e", "--extension", help='input log file type *Required unless file has extension in name')
+@click.option("--output-extension", help="output log file type *Required if file name does not include extension")
+
+def log(input_file, output_file, extension, output_extension):
+    '''
+    'convert log'   Converts input log file to a different format and writes to output
+
+    input-file      the input log file                                       
+    output-file     the output log file with extension
+    '''
+    log = open(input_file)
+    if extension:
+        if extension == 'txt':
+            return
+        elif extension == 'csv':
+            return
+        elif extension == 'log':
+            return
+        elif extension == 'blf':
+            return
+        elif extension== 'asc':
+            return
+        else: print('Extension type not provided or found')
+    else: print('Extension type not provided or found')
+    
 
 #@cli.command()
 
---
+++ carpwnd/carpwnd-tools/plotter.py
@@ -0,0 +1,8 @@
+import click
+import matplotlib
+
+@click.group()
+def cli():pass
+
+@cli.command()
+def plotall():pass
